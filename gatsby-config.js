592734ce9469a32076ea888f78a846cfbbfca9a4
Pruthe Thakkar <pruthe.thakkar@wockhardthospitals.com>
2026-05-07 14:53:41 +0530

    env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,33 +2,17 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: "Dr Manoj",
-    description: "Gatsby + WordPress (WPGraphQL) site",
-    author: "@Dr Manoj",
+    title: "Dr Manojkumar Gaddikeri",
+    description: "Orthopaedic Spine Specialist",
+    author: "@Dr Manojkumar Gaddikeri",
     siteUrl:
       process.env.GATSBY_WEBSITE_URL ||
-      "https://drmanoj.studiosentientdemo.com/",
+      "https://drmanojgaddikeri.netlify.app",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-source-wordpress",
-      options: {
-        url:
-          process.env.GATSBY_WPGRAPHQL_URL ||
-          "https://drmanoj.studiosentientdemo.com/m/graphql",
-        develop: {
-          hardCacheMediaFiles: true,
-          hardCacheData: false,
-          nodeUpdateInterval: 300000,
-        },
-        production: {
-          hardCacheMediaFiles: false,
-        },
-      },
-    },
   ],
 };
